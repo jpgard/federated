088b195a58703f1aeb5d600b96c69976174d65f2
jpgard <joshua.patrick.gardner@gmail.com>
2019-07-05 17:18:46 -0400

    removed hardcoded epochs in input_fn; will loop infinitely as necessary

--- src/estimator.py
+++ src/estimator.py
@@ -4,18 +4,20 @@ Creates a tf.estimator from input data
 
 import tensorflow as tf
 
-def get_dataset(file_path, batch_size=256, epochs=500):
+TRAIN_EPOCHS=1000
+
+def get_dataset(file_path, batch_size=256):
+    with open(file_path) as f:
+        first_line = f.readline()
+    headers = first_line.split(sep=",")
+    weekly_feats = [x for x in headers if x.startswith("week_")]
     dataset = tf.data.experimental.make_csv_dataset(
         # see https://www.tensorflow.org/versions/r1.13/api_docs/python/tf/data/experimental/make_csv_dataset
         file_path,
         batch_size=batch_size,  # Artificially small to make examples easier to show.
         label_name="label_value",
-        select_columns=["week_0_n_active_days",
-                        "week_0_n_forum_views",
-                        "week_0_num_posts",
-                        "label_value"],
+        select_columns= weekly_feats + ["label_value"],
         na_value="NaN",
-        num_epochs=epochs,
         shuffle=True)
     return dataset
 
@@ -47,4 +49,4 @@ estimator = tf.estimator.DNNClassifier(
 
 print("[INFO] training the model; to get tensorboard run tensorboard --logdir=PATH"
       "using the provided path in a terminal")
-estimator.train(input_fn=input_fn, steps=500)
+estimator.train(input_fn=input_fn, steps=TRAIN_EPOCHS)
