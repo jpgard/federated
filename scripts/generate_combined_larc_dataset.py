3141eaa0cb97df449bb69dcd9682d531a8e4b6ef
jpgard <joshua.patrick.gardner@gmail.com>
2020-01-06 13:21:33 -0800

    read directly from bz2 files

--- scripts/generate_combined_larc_dataset.py
+++ scripts/generate_combined_larc_dataset.py
@@ -3,29 +3,21 @@ Read the individual LARC tables and join them into a single student-term-course 
 
 Example usage:
 
-# mock LARC dataset
-python scripts/generate_combined_larc_dataset.py \
-    --prsn_identifying_info_fp data/larc-mock/PRSN_IDNTFYNG_INFO.csv \
-    --stdnt_info_fp data/larc-mock/STDNT_INFO.csv \
-    --stdnt_term_class_info_fp data/larc-mock/STDNT_TERM_CLASS_INFO.csv \
-    --stdnt_term_info_fp data/larc-mock/STDNT_TERM_INFO.csv \
-    --stdnt_term_trnsfr_info_fp data/larc-mock/STDNT_TERM_TRNSFR_INFO.csv \
-    --outdir data/larc-mock/
-
 # full LARC dataset (excluding transfer table)
 python scripts/generate_combined_larc_dataset.py \
-    --stdnt_info_fp data/LARC/LARC_20190924_STDNT_INFO.csv \
-    --stdnt_term_class_info_fp data/LARC/LARC_20190924_STDNT_TERM_CLASS_INFO.csv \
-    --stdnt_term_info_fp data/LARC/LARC_20190924_STDNT_TERM_INFO.csv \
+    --stdnt_info_fp data/LARC/LARC_20190924_STDNT_INFO.csv.bz2 \
+    --stdnt_term_class_info_fp data/LARC/LARC_20190924_STDNT_TERM_CLASS_INFO.csv.bz2 \
+    --stdnt_term_info_fp data/LARC/LARC_20190924_STDNT_TERM_INFO.csv.bz2 \
     --outdir data/larc-split/ \
     --generate_ttv
 """
 
-import pandas as pd
 import argparse
 import os
 import os.path as osp
+import bz2
 
+import pandas as pd
 
 TERM_CDS = {
     # the term codes to use for the validation dataset; should be the second-to-last full
@@ -41,6 +33,13 @@ TERM_CDS = {
         "2220"  # WN 2019
     ]
 }
+
+
+def read_csv_from_bz2(fp, index_cols, **read_csv_args):
+    print("[INFO] reading {}".format(fp))
+    with bz2.open(fp) as file:
+        df = pd.read_csv(file, index_col=index_cols, **read_csv_args)
+    return df
 
 
 def main(stdnt_info_fp,
@@ -59,7 +58,7 @@ def main(stdnt_info_fp,
     :param outdir:
     :param prsn_identifying_info_fp:
     :param stdnt_term_trnsfr_info_fp:
-    :param generate_ttv: boolean indicator for whether to generate train-test split.
+    :param generate_ttv: boolean indicator for whether to generate train-test-val split.
     :return:
     """
 
@@ -70,20 +69,17 @@ def main(stdnt_info_fp,
         "dtype": "object",
     }
 
-    print("[INFO] reading {}".format(stdnt_info_fp))
-    stdnt_info = pd.read_csv(stdnt_info_fp,
-                             index_col=("SNPSHT_RPT_DT", "STDNT_ID"),
-                             **read_csv_args)
-    print("[INFO] reading {}".format(stdnt_term_class_info_fp))
-    stdnt_term_class_info = pd.read_csv(stdnt_term_class_info_fp,
-                                        index_col=("SNPSHT_RPT_DT", "TERM_CD",
-                                                   "STDNT_ID", "CLASS_NBR"),
-                                        **read_csv_args)
-    # drop the duplicated column
-    print("[INFO] reading {}".format(stdnt_term_info_fp))
-    stdnt_term_info = pd.read_csv(stdnt_term_info_fp,
-                                  index_col=("SNPSHT_RPT_DT", "TERM_CD", "STDNT_ID"),
-                                  **read_csv_args).drop(columns="TERM_SHORT_DES")
+    stdnt_info = read_csv_from_bz2(
+        stdnt_info_fp, index_cols=("SNPSHT_RPT_DT", "STDNT_ID"), **read_csv_args)
+    stdnt_term_class_info = read_csv_from_bz2(
+        stdnt_term_class_info_fp,
+        index_cols=("SNPSHT_RPT_DT", "TERM_CD", "STDNT_ID", "CLASS_NBR"),
+        **read_csv_args)
+    # drop the duplicated and redundant column when reading stdnt_term_info
+    stdnt_term_info = read_csv_from_bz2(
+        stdnt_term_info_fp,
+        index_cols=("SNPSHT_RPT_DT", "TERM_CD", "STDNT_ID"),
+        **read_csv_args).drop(columns="TERM_SHORT_DES")
 
     # we want a student-term-class level dataset; join the other data onto this df
     larc = stdnt_term_class_info \
@@ -104,6 +100,7 @@ def main(stdnt_info_fp,
         print("[ERROR] transfer data join not implemented")
         raise NotImplementedError
     larc.reset_index(inplace=True)
+    import ipdbl;ipdb.set_trace()
     # ensure no data is lost or duplicated in joins
     assert len(larc) == len(stdnt_term_class_info)
     if not generate_ttv:  # write the entire dataset
@@ -123,10 +120,10 @@ def main(stdnt_info_fp,
                 os.mkdir(mode_out_dir)
             if mode == "train":
                 # filter the data using cutoff_term_cd
-                larc[larc["TERM_CD"].astype(int) < cutoff_term_cd]\
+                larc[larc["TERM_CD"].astype(int) < cutoff_term_cd] \
                     .to_csv(mode_out_fp, index=False)
             else:  # mode is val or test; write that data
-                larc[larc["TERM_CD"].isin(TERM_CDS[mode])]\
+                larc[larc["TERM_CD"].isin(TERM_CDS[mode])] \
                     .to_csv(mode_out_fp, index=False)
 
 
